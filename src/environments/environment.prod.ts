c325d19d93609fe14653cc2b924a0236369d08a9
Ariel Sto. Domingo <astodomingo@fullscale.io>
2024-01-14 16:03:37 +0800

    22th

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://oms-slhs.free.nf/public/api',
+  //apiUrl: 'https://oms-slhs.free.nf/public/api',
+  apiUrl: 'https://oms-slhs.online/public/api',
 };
